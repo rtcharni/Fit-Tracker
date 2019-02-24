ef2c5bd8f53241be921ed86a1e0b4213cc8b31f9
elama09 <roman.tcharni@gmail.com>
2019-02-24 11:26:55 +0200

    Modified weightscreen to get weightdata async and pass it down to components

--- screens/Weight/WeightScreen.js
+++ screens/Weight/WeightScreen.js
@@ -11,23 +11,29 @@ import {
   Button
 } from "react-native";
 import { WebBrowser } from "expo";
-import { SaveWeight, GetWeightArray, ClearAllWeights } from "../../utils/AsyncStorage";
+import {
+  SaveWeight,
+  GetWeightArray,
+  ClearAllWeights
+} from "../../utils/AsyncStorage";
 import WeightDataList from "./WeightDataList";
 
 export default class WeightScreen extends React.Component {
   static navigationOptions = {
-    title: 'Weight'
+    title: "Weight"
   };
 
   constructor() {
     super();
     this.state = {
-      enteredText: '',
+      enteredText: "",
       numberOK: false,
+      weightData: [],
     };
     this.handleChangeText = this.handleChangeText.bind(this);
     this.handleTextCheck = this.handleTextCheck.bind(this);
-    this.handleOnSaveButton = this.handleOnSaveButton.bind(this);
+    this.handleSaveButton = this.handleSaveButton.bind(this);
+    this.handleGetWeightsButton = this.handleGetWeightsButton.bind(this);
   }
 
   handleChangeText(value) {
@@ -35,50 +41,61 @@ export default class WeightScreen extends React.Component {
   }
 
   handleTextCheck() {
-    const indexOfDot1 = this.state.enteredText.indexOf('.');
+    const indexOfDot1 = this.state.enteredText.indexOf(".");
     if (!this.state.enteredText.length) {
-      this.setState({numberOK: false});
+      this.setState({ numberOK: false });
       return;
     } else if (indexOfDot1 !== -1) {
-      const indexOfDot2 = this.state.enteredText.indexOf('.', indexOfDot1 + 1);
+      const indexOfDot2 = this.state.enteredText.indexOf(".", indexOfDot1 + 1);
       if (indexOfDot2 !== -1) {
-        this.setState({numberOK: false})
+        this.setState({ numberOK: false });
         return;
       }
     }
-    this.setState({numberOK: true})
+    this.setState({ numberOK: true });
   }
 
-  async handleOnSaveButton() {
+  async handleSaveButton() {
     const time = new Date().getTime();
     const weight = parseFloat(this.state.enteredText);
-    const success = await SaveWeight({time, weight});
+    const success = await SaveWeight({ time, weight });
+  }
+
+  async handleGetWeightsButton() {
+    const weightData = await GetWeightArray();
+    this.setState({weightData});
+    // console.log(this.state.weightData)
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello from WeightScreen</Text>
-        <TextInput
-          onChangeText={this.handleChangeText}
-          maxLength={5}
-          keyboardType="decimal-pad"
-          placeholder="Enter weight (kg)"
-        />
-        <Text>{this.state.enteredText}</Text>
-        <Button
-          onPress={this.handleOnSaveButton}
-          disabled={this.state.numberOK === true ? false : true}
-          title="Save"
-          color="#841584"
-        />
-        <Text>{this.state.numberOK}</Text>
+      <View>
+        <View style={styles.container}>
+          <Text>Hello from WeightScreen</Text>
+          <TextInput
+            onChangeText={this.handleChangeText}
+            maxLength={5}
+            keyboardType="decimal-pad"
+            placeholder="Enter weight (kg)"
+          />
+          <Text>{this.state.enteredText}</Text>
+          <Button
+            onPress={this.handleSaveButton}
+            disabled={this.state.numberOK === true ? false : true}
+            title="Save"
+            color="#841584"
+          />
+          <Text>{this.state.numberOK}</Text>
+          {/* FOR TESTING */}
+          <Button title="Clear data" onPress={ClearAllWeights} />
+          <Button
+            title="Get Weights"
+            onPress={this.handleGetWeightsButton}
+          />
+          {/* FOR TESTING */}
+        </View>
 
-        {/* FOR TESTING */}
-        <Button title="Clear data" onPress={ClearAllWeights}></Button> 
-        <Button title="Get Weights" onPress={ async () => console.log(await GetWeightArray())} ></Button>
-        {/* FOR TESTING */}
-        <WeightDataList/>
+        <WeightDataList weightData={this.state.weightData} />
       </View>
     );
   }
