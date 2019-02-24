39ec8dabca98dc99e458cccd0202113862f98dbc
elama09 <roman.tcharni@gmail.com>
2019-02-24 11:24:49 +0200

    Added TESTDATA to project

--- screens/Weight/TESTDATA.js
+++ screens/Weight/TESTDATA.js
@@ -1,8 +1,8 @@
 export const TESTDATAWEIGHTS = [
     {time: 1550351018162, weight: 55},
     {time: 1550351078162, weight: 66},
-    {time: 1550955878162, weight: 56},
     {time: 1550861478162, weight: 78},
     {time: 1550869478162, weight: 45},
-    {time: 1550955878162, weight: 67}
+    {time: 1550955878162, weight: 67},
+    {time: 1551055878162, weight: 56},
 ]
