373773c9cd83a2efe99b067a5ddfe4d0217997f6
elama09 <roman.tcharni@gmail.com>
2019-03-09 20:05:44 +0200

    Added highcharts to project

--- screens/Links/LinksScreen.js
+++ screens/Links/LinksScreen.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import ChartView from 'react-native-highcharts';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Chart',
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        
       </ScrollView>
     );
   }
