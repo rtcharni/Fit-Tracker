41ac219e125be2ab7c4c48371b6657cf1c149410
elama09 <roman.tcharni@gmail.com>
2019-03-16 21:51:30 +0200

    feat(picture): Added picture to project and required it at app start

--- App.js
+++ App.js
@@ -23,8 +23,8 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           <Root>
-          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-          <AppNavigator />
+            {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+            <AppNavigator />
           </Root>
         </View>
       );
@@ -35,7 +35,8 @@ export default class App extends React.Component {
     return Promise.all([
       Asset.loadAsync([
         require("./assets/images/robot-dev.png"),
-        require("./assets/images/robot-prod.png")
+        require("./assets/images/robot-prod.png"),
+        require("./assets/images/apple.png"),
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
