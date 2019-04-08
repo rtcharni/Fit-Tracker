2820446fdac9b62b41412d68d09f5ae8f615c28d
elama09 <roman.tcharni@gmail.com>
2019-04-08 21:43:41 +0300

    Work in progress AddOrModifyExercise

--- screens/Exercise/AddOrModifyExercise.js
+++ screens/Exercise/AddOrModifyExercise.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {
   ListItem,
-  Icon as NativeElementIcon,
+  Icon as NativeElementsIcon,
   Overlay,
   Text as NativeElementsText
 } from "react-native-elements";
@@ -18,37 +18,53 @@ import {
   Icon as NativeBaseIcon,
   Item,
   Button,
-  Toast
+  Toast,
+  Picker
 } from "native-base";
 import window from "../../constants/Layout";
 
-export default class AddOrModifyWeight extends Component {
+export default class AddOrModifyExercise extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        showComponent: true,
-        editIcon: { editExerciseOK: false, success: false, error: true }
-    }
+      showEditModal: this.props.showEditModal,
+      editIcon: { editExerciseOK: false, success: false, error: true },
+      exercise: "",
+      duration: "",
+      intensity: "medium"
+    };
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.showEditModal !== prevState.showEditModal) {
+      return {
+        showEditModal: nextProps.showEditModal
+      };
+    }
     return null;
   }
 
-  handleChangeText(value) {
+  handleChangeText(textValue, target) {
+    if (target == "exercise")
+      this.setState({ exercise: textValue }, this.handleTextCheck);
+    if (target == "duration")
+      this.setState({ duration: textValue }, this.handleTextCheck);
     // this.setState({ editWeightValue: value }, this.handleTextCheck);
   }
 
   handleTextCheck() {
-    const regexWeightCheck = /[1-9][0-9]{0,2}[,.]?[0-9]{0,2}/;
-    const textCheckResult = this.state.editWeightValue.match(regexWeightCheck);
-    if (textCheckResult && textCheckResult[0] === textCheckResult.input) {
+    const regexDuration = /[1-9][0-9]{0,2}/;
+    const durationCheckResult = this.state.duration.match(regexDuration);
+    if (
+      durationCheckResult &&
+      durationCheckResult[0] === durationCheckResult.input
+    ) {
       this.setState({
-        editIcon: { editWeightOK: true, success: true, error: false }
+        editIcon: { editExerciseOK: true, success: true, error: false }
       });
     } else {
       this.setState({
-        editIcon: { editWeightOK: false, success: false, error: true }
+        editIcon: { editExerciseOK: false, success: false, error: true }
       });
     }
   }
@@ -61,26 +77,24 @@ export default class AddOrModifyWeight extends Component {
     }
   }
 
-//   resetComponent() {
-//     this.setState({
-//       editWeightValue: "",
-//       chosenWeightItem: null,
-//       editIcon: {
-//         editWeightOK: false,
-//         success: false,
-//         error: true
-//       }
-//     });
-//   }
+  resetComponent() {
+    this.setState({
+      exercise: "",
+      duration: "",
+      intensity: "medium",
+      // chosenWeightItem: null,
+      editIcon: { editExerciseOK: false, success: false, error: true }
+    });
+  }
 
   render() {
     return (
       <Overlay
-        isVisible={this.state.showComponent}
-        height={window.window.height - window.window.height / 1.6}
+        isVisible={this.state.showEditModal}
+        height={window.window.height - window.window.height / 2.1}
         onBackdropPress={() => {
-        //   this.resetComponent();
-        //   this.props.closeEnterWeightWindow();
+          this.resetComponent();
+          this.props.closeEditModal();
         }}
         animationType="slide"
         transparent={true}
@@ -92,24 +106,66 @@ export default class AddOrModifyWeight extends Component {
             <Card>
               <CardItem header bordered>
                 <NativeBaseText>
-                  {/* {this.state.chosenWeightItem
-                    ? "Enter new weight"
-                    : "Enter weight"} */}
+                  {this.state.chosenExerciseItem
+                    ? "Edit exercise"
+                    : "Enter new exercise"}
                 </NativeBaseText>
               </CardItem>
               <CardItem bordered>
                 <Body>
                   <Item
-                    error={this.state.editIcon.error}
-                    success={this.state.editIcon.success}
+                  // error={this.state.editIcon.error}
+                  // success={this.state.editIcon.success}
                   >
                     <Input
-                      placeholder="Weight..."
-                      onChangeText={this.handleChangeText}
-                      maxLength={5}
+                      autoFocus
+                      ref={c => (this.exerciseInput = c)}
+                      placeholder="Exercise name..."
+                      onChange={e =>
+                        this.handleChangeText(e.nativeEvent.text, "exercise")
+                      }
+                      maxLength={30}
+                      keyboardType="default"
+                      returnKeyType="next"
+                      blurOnSubmit={false}
+                      onSubmitEditing={() => {
+                        // console.log(this.secondInput);
+                        this.durationInput._root.focus();
+                      }}
+                    />
+                  </Item>
+                  <Item
+                  error={this.state.editIcon.error}
+                  success={this.state.editIcon.success}
+                  >
+                    <Input
+                      ref={c => (this.durationInput = c)}
+                      placeholder="Duration (min)..."
+                      nativeID="durationField"
+                      onChange={e =>
+                        this.handleChangeText(e.nativeEvent.text, "duration")
+                      }
+                      maxLength={3}
                       keyboardType="decimal-pad"
+                      returnKeyType="done"
                     />
                     <NativeBaseIcon name={this.getIconOkOrError()} />
+                  </Item>
+                  <Item picker>
+                    <Picker
+                      ref={c => (this.intensityInput = c)}
+                      mode="dropdown"
+                      iosIcon={<NativeBaseIcon name="arrow-down" />}
+                      style={{ width: undefined }}
+                      placeholderStyle={{ color: "#bfc6ea" }}
+                      placeholderIconColor="#007aff"
+                      selectedValue={this.state.intensity}
+                      onValueChange={intensity => this.setState({ intensity })}
+                    >
+                      <Picker.Item label="Low" value="low" />
+                      <Picker.Item label="Medium" value="medium" />
+                      <Picker.Item label="High" value="high" />
+                    </Picker>
                   </Item>
 
                   <View
@@ -131,10 +187,10 @@ export default class AddOrModifyWeight extends Component {
                       </NativeBaseText>
                     </Button>
                     <Button
-                    //   onPress={() => {
-                    //     this.resetComponent();
-                    //     this.props.closeEnterWeightWindow();
-                    //   }}
+                      onPress={() => {
+                        this.resetComponent();
+                        this.props.closeEditModal();
+                      }}
                       rounded
                       bordered
                       dark
