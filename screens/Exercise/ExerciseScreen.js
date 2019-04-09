f20f7f122990a4f1be7286ccec8f78487df598a7
elama09 <roman.tcharni@gmail.com>
2019-04-09 20:10:12 +0300

    Work on exercisescreen

--- screens/Exercise/ExerciseScreen.js
+++ screens/Exercise/ExerciseScreen.js
@@ -5,6 +5,7 @@ import { ExpoConfigView } from "@expo/samples";
 import ExerciseDataList from "./ExerciseDataList";
 import Colors from "../../constants/Colors";
 import AddOrModifyExercise from "./AddOrModifyExercise";
+import { GetExerciseArray } from "../../utils/AsyncStorage";
 
 export default class ExerciseScreen extends React.Component {
   static navigationOptions = {
@@ -13,19 +14,33 @@ export default class ExerciseScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {showEditModal: false};
+    this.state = { showEditModal: false, exercises: [] };
     this.closeEditModal = this.closeEditModal.bind(this);
+    this.getAllExercises = this.getAllExercises.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllExercises();
   }
   // TODO next add AddOrModifyExercise component to this component, and workwork
   closeEditModal() {
-    this.setState({showEditModal: false})
+    this.setState({ showEditModal: false });
+  }
+
+  async getAllExercises() {
+    const allExercises = await GetExerciseArray();
+    this.setState({ exercises: allExercises });
   }
 
   render() {
     return (
       <ScrollView style={{ flex: 1 }}>
-      <AddOrModifyExercise showEditModal={this.state.showEditModal} closeEditModal={this.closeEditModal}/>
-      <View
+        <AddOrModifyExercise
+          showEditModal={this.state.showEditModal}
+          closeEditModal={this.closeEditModal}
+          getAllExercises={this.getAllExercises}
+        />
+        <View
           style={{
             flex: 1,
             flexDirection: "row",
@@ -53,12 +68,11 @@ export default class ExerciseScreen extends React.Component {
             size={24}
             iconStyle={{}}
             containerStyle={{}} // alignSelf: "flex-end"
-            onPress={() => this.setState({showEditModal: true})}
+            onPress={() => this.setState({ showEditModal: true })}
           />
         </View>
-        <ExerciseDataList/>
+        <ExerciseDataList exercises={this.state.exercises} getAllExercises={this.getAllExercises}/>
       </ScrollView>
-      
     );
   }
 }
